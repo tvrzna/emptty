1b8b68567aa89b98e9296d03f7c4aa1ba45296e6
Michal Tvrznik <emporeor@gmail.com>
2020-05-13 20:52:49 +0200

    Reworked configuration to use environment variables instead of arguments/flags.

--- README.md
+++ README.md
@@ -1,39 +1,52 @@
 # emptty
 Dead simple Display Manager running in CLI as TTY login, that starts .xinitrc or .winitrc.
 
-## Build dependencies
-- go
-- gcc
-- pam-devel
+### Configuration
+Configuration is handled via environment variables.
 
-## Dependencies
-- pam
-- xorg (optional)
-- xauth (required for xorg)
-- mcookie (required for xorg)
-- wayland (optional)
+`TTY_NUMBER` TTY, where emptty will start.
 
-## .xinitrc sample
+`DEFAULT_USER` Preselected user, if AUTOLOGIN is enabled, this user is logged in.
+
+`AUTOLOGIN` Enables Autologin, if DEFAULT_USER is defined. Possible values are "true" or "false".
+
+`ENVIRONMENT` Selects, which environment will be used. Possible values are "xorg" or "wayland".
+- If "xorg" is selected, it expects to have prepared .xinitrc file with +x at home folder.
+- If "wayland" is selected, it expects to have prepared .winitrc file with +x at home folder.
+
+### .xinitrc sample
 ```
 #!/bin/sh
 export LANG=en_US.UTF-8
 exec dbus-launch i3
 ```
 
-## .winitrc sample
+### .winitrc sample
 ```
 #!/bin/sh
 export LANG=en_US.UTF-8
 exec dbus-launch sway
 ```
 
+### Build dependencies
+- go
+- gcc
+- pam-devel
+
+### Dependencies
+- pam
+- xorg (optional)
+- xauth (required for xorg)
+- mcookie (required for xorg)
+- wayland (optional)
+
 ---
-### TODO:
+#### TODO:
 - [x] PAM
 - [x] Xorg support
 - [x] runit service
 - [x] Common failure handling
-- [x] Configuration/Arguments
+- [x] Configuration
 - [x] Wayland
 - [x] Autologin/Preset username
 - [ ] systemd service
--- config.go
+++ config.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"flag"
+	"os"
+	"strconv"
+	"strings"
 )
 
-// EnEnvironment defines possible Environments
+// EnEnvironment defines possible Environments.
 type EnEnvironment int
 
 const (
@@ -13,6 +15,13 @@ const (
 
 	// Wayland represents Wayland environment
 	Wayland
+)
+
+const (
+	envTTYnumber   = "TTY_NUMBER"
+	envDefaultUser = "DEFAULT_USER"
+	envAutologin   = "AUTOLOGIN"
+	envEnvironment = "ENVIRONMENT"
 )
 
 // Config defines structure of application configuration.
@@ -27,26 +36,34 @@ type Config struct {
 func LoadConfig() *Config {
 	c := Config{}
 
-	tty := flag.Int("t", 7, "used TTY")
-	env := flag.String("e", "xorg", "environment to be started")
-	usr := flag.String("u", "", "preselected user")
-	autologin := flag.Bool("a", false, "autologin to user, expects to preselect user")
-
-	flag.Parse()
-
-	c.environment = parseEnv(*env)
-	c.defaultUser = *usr
-	c.tty = *tty
-	if *autologin && c.defaultUser != "" {
-		c.autologin = *autologin
+	c.environment = parseEnv(os.Getenv(envEnvironment))
+	c.tty = parseTTY(os.Getenv(envTTYnumber))
+	c.defaultUser = parseDefaultUser(os.Getenv(envDefaultUser))
+	autologin := parseAutologin(os.Getenv(envAutologin))
+	if autologin && c.defaultUser != "" {
+		c.autologin = autologin
 	}
+
+	os.Unsetenv(envEnvironment)
+	os.Unsetenv(envTTYnumber)
+	os.Unsetenv(envDefaultUser)
+	os.Unsetenv(envAutologin)
 
 	return &c
 }
 
+// Parse TTY number.
+func parseTTY(tty string) int {
+	val, err := strconv.ParseInt(sanitizeValue(tty, "7"), 10, 32)
+	if err != nil {
+		return 7
+	}
+	return int(val)
+}
+
 // Parse input env and selects corresponding environment.
 func parseEnv(env string) EnEnvironment {
-	switch env {
+	switch sanitizeValue(env, "xorg") {
 	case "wayland":
 		return Wayland
 	case "xorg":
@@ -54,4 +71,26 @@ func parseEnv(env string) EnEnvironment {
 		return Xorg
 	}
 	return Xorg
+}
+
+// Parse, if autologin is enabled.
+func parseAutologin(autologin string) bool {
+	val, err := strconv.ParseBool(sanitizeValue(autologin, "false"))
+	if err != nil {
+		return false
+	}
+	return val
+}
+
+// Parse default user.
+func parseDefaultUser(defaultUser string) string {
+	return sanitizeValue(defaultUser, "")
+}
+
+// Sanitize value.
+func sanitizeValue(value string, defaultValue string) string {
+	if value == "" {
+		return defaultValue
+	}
+	return strings.TrimSpace(value)
 }
