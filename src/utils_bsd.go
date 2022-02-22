0fa162b68d79350f652c599e7d93aff5abd08067
Michal Tvrznik <emporeor@gmail.com>
2022-02-22 10:01:21 +0100

    Removed unnecessary `password_bsd.go` and `password_linux.go` since setTerminalEcho method could be placed into `utils`.

--- src/utils_bsd.go
+++ src/utils_bsd.go
@@ -20,3 +20,16 @@ func setFsUser(usr *sysuser) {
 func setKeyboardLeds(tty *os.File, scrolllock, numlock, capslock bool) {
 	// Not implemented yet
 }
+
+// Enables or disables echo depending on status
+func setTerminalEcho(fd uintptr, status bool) error {
+	flag := ""
+	if !status {
+		flag = "-"
+	}
+	pid, err := syscall.ForkExec("/bin/stty", []string{"stty", flag + "echo"}, &syscall.ProcAttr{Dir: "", Files: []uintptr{fd}})
+	if err == nil {
+		syscall.Wait4(pid, nil, 0, nil)
+	}
+	return err
+}
--- src/utils_linux.go
+++ src/utils_linux.go
@@ -54,3 +54,23 @@ func setKeyboardLeds(tty *os.File, scrolllock, numlock, capslock bool) {
 		syscall.Syscall(syscall.SYS_IOCTL, uintptr(tty.Fd()), uintptr(_KDSETLED), uintptr(flags))
 	}
 }
+
+// Enables or disables echo depending on status
+func setTerminalEcho(fd uintptr, status bool) error {
+	var termios = &syscall.Termios{}
+
+	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, syscall.TCGETS, uintptr(unsafe.Pointer(termios))); err != 0 {
+		return err
+	}
+
+	if status {
+		termios.Lflag |= syscall.ECHO
+	} else {
+		termios.Lflag &^= syscall.ECHO
+	}
+
+	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(termios))); err != 0 {
+		return err
+	}
+	return nil
+}
